f40f8a671a93c7c44ed0e4cfd5ae813b2df48c91
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-08-22 16:05:35 +0300

    chore: create infrastructure layer

---
+++ Infrastructure/InfrastructureServices.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class InfrastructureServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
