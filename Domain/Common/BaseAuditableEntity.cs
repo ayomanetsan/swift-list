1c28d65af887ae61bccb16f67b3a4277559c410d
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-08-23 07:46:02 +0300

    chore: create users table

---
+++ Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Common
+{
+    public abstract class BaseAuditableEntity
+    {
+        public Guid Id { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public string? CreatedBy { get; set; }
+    }
+}
--- Infrastructure/Data/ApplicationDbContext.cs
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Infrastructure.Data
@@ -6,6 +7,8 @@ namespace Infrastructure.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
