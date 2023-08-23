3f3d13c53ae79e2698baf84bd567d34939c9e165
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2023-08-23 22:07:07 +0300

    feat: update auth validation

---
+++ Application/Users/Queries/Login/LoginQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Queries.Login
+{
+    public class LoginQueryValidator : AbstractValidator<LoginQuery>
+    {
+        public LoginQueryValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Provided email is not valid.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+                .MaximumLength(32).WithMessage("Password must not exceed 32 characters.");
+        }
+    }
+}
