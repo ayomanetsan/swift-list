16f573218f6709a08461de6e0299e18f518a07ed
ayomanetsan <vladyslav.voropaiev1@nure.ua>
2024-03-31 18:56:13 +0300

    chore: remove swagger

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -12,9 +12,6 @@ builder.Configuration.AddUserSecrets<Program>();
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer();
@@ -48,13 +45,6 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
